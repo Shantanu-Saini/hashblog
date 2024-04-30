67bb5e8b99051190cd551a552855b5a1e7dafbe9
Shantanu <shantanusaini7000@gmail.com>
2024-04-30 11:34:01 +0530

    set up react-redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import conf from './conf/config.js';
+import { Header, Footer } from "./components/index.js"
+import { useDispatch } from 'react-redux';
+import authService from './appwrite/auth.user.js';
+import { login, logout } from './store/authSlice.js';
+import { Outlet } from 'react-router-dom';
 
 const App = () => {
-  console.log(conf.appwriteProjectId);
-  return (
-    <>
-      <h1>Hello  World!</h1>
-    </>
-  )
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login(userData));
+        }
+        else {
+          dispatch(logout());
+        }
+      })
+      .catch(() => console.warn('No user data found'))
+      .finally(() => setLoading(false));
+  }, [])
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-700'>
+      <div className='w-full block text-white'>
+        <Header />
+        <main>
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App;
